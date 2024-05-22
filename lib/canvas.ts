e829304bcde4a07af0f165437ba19529f9009d73
nageshmali <malinagesh60@gmail.com>
2024-05-22 18:27:03 +0530

    done with acc canvas live editing

--- lib/canvas.ts
+++ lib/canvas.ts
@@ -382,7 +382,7 @@ export const renderCanvas = ({
 };
 
 // resize canvas dimensions on window resize
-export const handleResize = ({ canvas }: { canvas: fabric.Canvas | null }) => {
+export const handleResize = ({ canvas }:any| { canvas: fabric.Canvas | null }) => {
   const canvasElement = document.getElementById("canvas");
   if (!canvasElement) return;
 
